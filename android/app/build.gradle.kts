4c870033d62c2158922c7ff846570b106aa7e0a6
Secretuser129 <tvedansh569@gmail.com>
2026-07-26 00:24:34 +0530

    Update version code to 209 and version name to 2.0.9 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.offpay"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 20005
-        versionName = "2.0.5"
+        versionCode = 209
+        versionName = "2.0.9"
     }
 
     compileOptions {
